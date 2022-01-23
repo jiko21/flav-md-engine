90bf361fc8cee3839eb843640e96dcd642b17375
jiko21 <daikikojima0531@icloud.com>
2022-01-23 21:13:11 +0900

    add: enclose function for quote

--- src/lexer/pattern/quote.rs
+++ src/lexer/pattern/quote.rs
@@ -1,6 +1,25 @@
 pub mod quote {
+    use once_cell::sync::Lazy;
+    use regex::Regex;
+
+    static CODE_BLOCK_PAREN_PATTERN: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"^>\s*([\s>]*.+)").unwrap());
+
     pub fn is_quote_block(input: &String) -> bool {
         input.chars().nth(0).unwrap() == '>'
+    }
+
+    pub fn enclose_quote(input: Vec<String>) -> Vec<String> {
+        input
+            .iter()
+            .map(|s| {
+                let rslt = CODE_BLOCK_PAREN_PATTERN.captures(s);
+                match rslt {
+                    Some(c) => c.get(1).map_or(s.to_string(), |m| m.as_str().to_string()),
+                    None => s.to_string(),
+                }
+            })
+            .collect::<Vec<String>>()
     }
 
     #[cfg(test)]
@@ -33,6 +52,20 @@ pub mod quote {
                 let output = is_quote_block(&test_case.input);
                 assert_eq!(output, test_case.expected, "Failed: {}\n", test_case.it);
             }
+        }
+
+        #[test]
+        fn test_enclose_quote() {
+            let input = vec!["> hogehoge", "> this is test", "aaa"]
+                .iter()
+                .map(|s| s.to_string())
+                .collect();
+            let expected: Vec<String> = vec!["hogehoge", "this is test", "aaa"]
+                .iter()
+                .map(|s| s.to_string())
+                .collect();
+            let output = enclose_quote(input);
+            assert_eq!(output, expected);
         }
     }
 }
